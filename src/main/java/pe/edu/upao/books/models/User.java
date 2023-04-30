4e18b7e11d6e3fd84eb2385085e544d98465e849
Carlos Ruiz <carlossruizr@gmail.com>
2023-04-30 01:22:43 -0500

    Implementación de librería lombok para la lógica correspondiente de UserServiceImpl

--- src/main/java/pe/edu/upao/books/models/User.java
+++ src/main/java/pe/edu/upao/books/models/User.java
@@ -1,12 +1,19 @@
 package pe.edu.upao.books.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 
 @Entity
 @Table(name="users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
